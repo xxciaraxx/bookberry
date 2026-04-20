6e0cc57f5864b358d89ec16d5524a343d2d1d951
xxciaraxx <ciaralaranang3@gmail.com>
2026-04-20 22:57:50 +0800

    bruh

--- resources/views/components/auth-layout.blade.php
+++ resources/views/components/auth-layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>{{ $title ?? 'BookBerry' }}</title>
+    <link rel="icon" type="image/jpeg" href="{{ asset('images/bookberry-logo.svg') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
     <style>
